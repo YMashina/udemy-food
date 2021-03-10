2f884534bab45d90346c5305a2955df48bccec45
Yulia <lethanavir@gmail.com>
2021-03-10 22:47:35 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+tabItems = document.querySelectorAll('.tabheader__item');
+tabContentItems = document.querySelectorAll('.tabcontent');
+tabs = {
+    fitnessTabheader : 'fitnessTabcontent',
+    eliteTabheader: 'eliteTabcontent',
+    vegetarianTabheader: 'vegetarianTabcontent',
+    balanceTabheader: 'balancedTabcontent',
+};
+tabItems.forEach((tabItem)=>{
+    tabItem.addEventListener('click',(event)=>{
+        console.log(event.target.classList);
+        event.target.classList.add('tabheader__item_active');
+        console.log(event.target.classList);
+        tabItems.forEach((element) => {
+            if (element !== event.target && element.classList.contains('tabheader__item_active'))
+                element.classList.remove('tabheader__item_active');
+        });
+        tabContentItems.forEach((element) => {
+            element.classList.add('not_displayed');
+        });
+        document.getElementById(tabs[event.target.id]).classList.remove('not_displayed');
+    });
+});
+
+document.getElementById('modalContactClose').addEventListener('click', () => {
+    document.getElementById('modalContact').style.display = 'none';
+});
+
+contactUsBtns = document.querySelectorAll('.btn_contact_us');
+contactUsBtns.forEach((element)=>{
+    element.addEventListener('click', (event) => {
+        document.getElementById('modalContact').style.display = 'block';
+    });
+});
