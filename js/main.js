4bb7bf42bc876a3896791fe6873059e4f3901e60
Yulia <lethanavir@gmail.com>
2021-03-11 18:32:35 +0300

    add timer

--- js/main.js
+++ js/main.js
@@ -22,13 +22,49 @@ tabItems.forEach((tabItem)=>{
     });
 });
 
-document.getElementById('modalContactClose').addEventListener('click', () => {
+function scrollOpenModal () {
+    if( (window.innerHeight + window.scrollY) >= document.body.offsetHeight ){
+        openModal();
+        window.removeEventListener('scroll',scrollOpenModal);
+    }
+}
+
+const openModal = () => {
+    document.getElementById('modalContact').style.display = 'block';
+    clearTimeout(openModalTimer);
+};
+
+const closeModal = () => {
     document.getElementById('modalContact').style.display = 'none';
+};
+
+document.getElementById('modalContactClose').addEventListener('click', () => {
+    closeModal();
 });
 
 contactUsBtns = document.querySelectorAll('.btn_contact_us');
 contactUsBtns.forEach((element)=>{
     element.addEventListener('click', (event) => {
-        document.getElementById('modalContact').style.display = 'block';
+        openModal();
     });
 });
+
+window.addEventListener('scroll', scrollOpenModal);
+
+const endDate = new Date(2021, 4, 10, 10, 0, 0);
+const openModalTimer = setTimeout(openModal, 5000);
+
+const timeUpdaterInterval = setInterval(()=>{
+    const currentDate = new Date();
+    let difference = endDate - currentDate;
+    if (difference === 0)
+        clearInterval(timeUpdaterInterval);
+    const daysLeft = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hoursLeft = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesLeft = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsLeft = Math.floor((difference % (1000 * 60)) / 1000);
+    document.getElementById('days').innerText = daysLeft.toString();
+    document.getElementById('hours').innerText = hoursLeft.toString();
+    document.getElementById('minutes').innerText = minutesLeft.toString();
+    document.getElementById('seconds').innerText = secondsLeft.toString();
+}, 1000);
