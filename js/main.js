8724f92cdf0edfeff146633c337d1a444e9d4511
YMashina <lethanavir@gmail.com>
2021-04-05 13:56:05 +0300

    calculator +localstorage done

--- js/main.js
+++ js/main.js
@@ -325,6 +325,11 @@ const sliderCarouselFunction = (slides) => {
     });
 };
 
+
+let weight = document.querySelector('#weight');
+let height = document.querySelector('#height');
+let age = document.querySelector('#age');
+const femaleGender = document.querySelector('#gender').firstElementChild;
 const calculatorResult = document.querySelector('.calculating__result').querySelector('span');
 const physicalActivity = [
     1.2,
@@ -334,9 +339,32 @@ const physicalActivity = [
     1.9,
 ];
 
+if (localStorage.getItem('weight'))
+    weight.value = localStorage.getItem('weight');
+if (localStorage.getItem('height'))
+    height.value = localStorage.getItem('height');
+if (localStorage.getItem('age'))
+    age.value = localStorage.getItem('age');
+if (localStorage.getItem('gender') === 'male'){
+    femaleGender.classList.remove('calculating__choose-item_active');
+    femaleGender.nextElementSibling.classList.add('calculating__choose-item_active');
+}
+if (localStorage.getItem('activityType')){
+    const type = Number(localStorage.getItem('activityType'));
+    let typeIndex = 0;
+    for (let activityType of document.querySelector('#physicalActivityType').children){
+        activityType.classList.remove('calculating__choose-item_active');
+        if (type === typeIndex)
+            activityType.classList.add('calculating__choose-item_active');
+        typeIndex++;
+    }
+}
+if (localStorage.getItem('result'))
+    calculatorResult.innerText = localStorage.getItem('result');
+
+
 const updateCalculatorResult = () => {
     let result;
-    const femaleGender = document.querySelector('#gender').firstElementChild;
     let type;
     let typeIndex = 0;
     for (let activityType of document.querySelector('#physicalActivityType').children){
@@ -349,7 +377,14 @@ const updateCalculatorResult = () => {
     let weight = document.querySelector('#weight').value.replace(/[^0-9]/g, '');
     let height = document.querySelector('#height').value.replace(/[^0-9]/g, '');
     let age = document.querySelector('#age').value.replace(/[^0-9]/g, '');
-    console.log(height, weight, age)
+    localStorage.setItem('weight', weight);
+    localStorage.setItem('height', height);
+    localStorage.setItem('age', age);
+    localStorage.setItem('activityType', type);
+    if (femaleGender.classList.contains('calculating__choose-item_active'))
+        localStorage.setItem('gender', 'female');
+    else
+        localStorage.setItem('gender', 'male');
     if (!(weight !== '' && height !== '' && age !== '' ))
         return 0;
     if (femaleGender.classList.contains('calculating__choose-item_active')){
@@ -358,21 +393,19 @@ const updateCalculatorResult = () => {
         result = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * physicalActivity[type];
     }
     if (!isNaN(result)){
+        localStorage.setItem('result', Math.round(result).toString());
         return Math.round(result);
     }
     else
         return 0;
 };
 
-let weight = document.querySelector('#weight');
-let height = document.querySelector('#height');
-let age = document.querySelector('#age');
 
 let parametersArray = [height, weight, age];
 parametersArray.forEach((parameter)=>{
     parameter.addEventListener('input', ()=>{
         parameter.value = parameter.value.replace(/[^0-9]/g, '');
-        calculatorResult.innerText = updateCalculatorResult();
+        calculatorResult.innerText = updateCalculatorResult().toString();
     });
 
 });
@@ -385,7 +418,7 @@ calculatorChooseItems.forEach((element) => {
                 child.classList.remove('calculating__choose-item_active');
             }
             element.classList.add('calculating__choose-item_active');
-            calculatorResult.innerText = updateCalculatorResult();
+            calculatorResult.innerText = updateCalculatorResult().toString();
         });
     }
 });
